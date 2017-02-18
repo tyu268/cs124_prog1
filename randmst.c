8423a32dd47066c82a94db91d86b3fab9dc43a80
Tiffany Yu <tiffanyyu268@gmail.com>
2017-02-18 17:26:24 -0500

    testing args str to int

--- randmst.c
+++ randmst.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
 int main( int argc, const char* argv[] )
 {
 	printf( "Hello World!\n" );
 
-  // Prints each argument on the command line.
-  int i;
-	for( i = 0; i < argc; i++ )
+  if( argc > 1 )
 	{
-		printf( "arg %d: %s\n", i, argv[i] );
+		int i;
+		for (i = 1; i < argc; i++)
+		{
+			// figure out how to check for correct input
+			int arg = strtol(argv[i], NULL, 10);
+			printf( "arg %d: %d\n", i, arg);
+		}
 	}
+
 }
