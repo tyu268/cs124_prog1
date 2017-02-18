592ca06011420affe20e4b08b5a32562b73a25b1
Michelle Chiang <michellechiang@college.harvard.edu>
2017-02-18 18:33:18 -0500

    adjacency matrix

--- graph1.c
+++ graph1.c
@@ -2,11 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-	
-	// generating random numbers
-	srand((unsigned int)time(NULL));
-	double random = rand() / (double) RAND_MAX;
+double random_number() {
+	double random = rand() / (double) RAND_MAX;	
+	return random;
+}
 
-	printf("%f\n", random);
+int main(int argc, const char* argv[]) {
+	
+	int n = strtol(argv[1], NULL, 10);
+	double matrix[n][n];
+	int i = 0, j = 0; 
+
+	srand((unsigned int)time(NULL));
+
+	// create adjacency matrix
+	for (i = 0; i < n; i++) {
+		for (j = i; j < n; j++) {
+			matrix[i][j] = matrix[j][i] = random_number();
+			printf("%f\n", matrix[i][j]);
+		}
+	} 
+
 }
