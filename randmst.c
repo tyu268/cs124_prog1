9ecb91d319c43d9137bf8a196aebcd7d14241762
Tiffany Yu <tiffanyyu268@gmail.com>
2017-02-23 00:25:37 -0500

    input validation in randmst

--- randmst.c
+++ randmst.c
@@ -5,18 +5,16 @@
 
 int main( int argc, const char* argv[] )
 {
-	printf( "Hello World!\n" );
-
-  if( argc > 1 )
-	{
-		int i;
-		for (i = 1; i < argc; i++)
-		{
-			// figure out how to check for correct input
-			int arg = strtol(argv[i], NULL, 10);
-			printf( "arg %d: %d\n", i, arg);
-		}
+	if (argc < 5) {
+		printf("Invalid input\n");
+		return 1;
 	}
+  int numpoints = strtol(argv[2], NULL, 10);
+	int numtrials = strtol(argv[3], NULL, 10);
+	int dimension = strtol(argv[4], NULL, 10);
+	printf("numpoints: %d; numtrials: %d; dimension: %d\n",
+					numpoints, numtrials, dimension);
+
 	int* arr = malloc(sizeof(int) * 9);
 	int i;
 	for (i = 0; i < 9; i++) {
