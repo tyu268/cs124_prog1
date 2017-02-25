284277e94d5d6a1d70868b49dca1bfb1736697dc
Tiffany Yu <tiffanyyu268@gmail.com>
2017-02-25 13:27:42 -0500

    added skeleton code to randmst

--- randmst.c
+++ randmst.c
@@ -15,10 +15,43 @@ int main( int argc, const char* argv[] )
 	printf("numpoints: %d; numtrials: %d; dimension: %d\n",
 					numpoints, numtrials, dimension);
 
+	double avg_weight = 0.0;
+
 	int* arr = malloc(sizeof(int) * 9);
 	int i;
 	for (i = 0; i < 9; i++) {
 		arr[i] = 0;
 	}
 	printArr(arr, 9);
+
+	// array for graph
+	double** g;;
+
+	switch(dimension) {
+		case 0 :
+			// simple random matrix
+			break;
+		case 2 :
+			// unit square
+			break;
+		case 3 :
+			// unit cube
+			break;
+		case 4 :
+			// unit hypercube
+			break;
+		defaultx :
+			printf("Invalid dimension\n");
+			return 1;
+	}
+
+	for (i = 0; i < numtrials; i++) {
+		// re-initialize graph with numpoints points on every iteration
+		// run prim on graph
+		// run weight on array returned by prim, and add result to avg_weight
+	}
+	avg_weight /= numpoints;
+	printf("Average weight for %d points for %d trials "
+				 "with graph dimension %d: %f\n",
+					numpoints, numtrials, dimension, avg_weight);
 }
