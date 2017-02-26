151cceb08ea23933d267edc32869818f1645dd9a
Tiffany Yu <tiffanyyu268@gmail.com>
2017-02-25 23:41:32 -0500

    added print statements

--- randmst.c
+++ randmst.c
@@ -55,14 +55,17 @@ int main( int argc, const char* argv[] )
 		// re-initialize graph with numpoints points on every iteration
 		graph_func(g, numpoints);
 		// run prim on graph
-		//printf("Graph created\n");
+		printf("Graph created\n");
 		int* edges = malloc(numpoints * sizeof(int));
 		edges =	prim(g, numpoints);
-		//printf("prim completed\n");
+		printf("prim completed\n");
 		// run weight on array returned by prim, and add result to avg_weight
 		avg_weight += weight(g, edges, numpoints);
-		//printf("avg weight added\n");
+		printf("avg weight added\n");
 		printf("Trial %d complete\n", i);
+		end = clock();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("Time so far: %f\n", cpu_time_used);
 	}
 	avg_weight /= numtrials;
 	printf("Average weight for %d points for %d trials "
